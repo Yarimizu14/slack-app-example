66d2159761b04c26a91c99e40abe84b47e4bd743
Kensei Yarimizu <kensei@abeja.asia>
2020-09-29 01:26:34 +0900

    switched database to postgresql and added docker-compose

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_09_28_131652) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", id: false, force: :cascade do |t|
     t.string "id"
     t.text "access_token"
