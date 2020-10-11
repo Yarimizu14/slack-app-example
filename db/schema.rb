d15fcb54b279e51def222aa4d4b4dc05f78e6acc
Kensei Yarimizu <kensei@abeja.asia>
2020-10-11 19:28:38 +0900

    added project resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_28_131652) do
+ActiveRecord::Schema.define(version: 2020_10_11_102543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", id: false, force: :cascade do |t|
     t.string "id"
