2aad19a48c0e336427cf93bd64ca8cdfbca92948
Kensei Yarimizu <kensei@abeja.asia>
2020-10-13 03:18:18 +0900

    update users table on Slack callback endpoint

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,10 +1,22 @@
 
 class AuthController < ApplicationController
 
+  def login
+    session[:user_id] = params[:user_id]
+    redirect_to "/auth/slack"
+  end
+
   # https://github.com/omniauth/omniauth#integrating-omniauth-into-your-application
   def callback
-    @user = User.find_or_create_from_auth_hash(access_token['authed_user'])
-    render plain: { message: 'ok', user_id: @user.id }.to_json
+    user_id = session[:user_id]
+    @user = User.find(user_id)
+
+    slack_user = access_token['authed_user']
+    if @user.update(slack_id: slack_user['id'], access_token: slack_user['access_token'])
+      render plain: { message: 'ok', user_id: @user.id }.to_json
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
   end
 
   protected
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_12_173546) do
+ActiveRecord::Schema.define(version: 2020_10_12_175504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_10_12_173546) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "organization_id", null: false
     t.string "name", null: false
+    t.string "slack_id"
     t.index ["organization_id"], name: "index_users_on_organization_id"
   end
 
