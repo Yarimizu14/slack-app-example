e39253cde2365ac0fb9f6243643f2679f0153077
Kensei Yarimizu <kensei@abeja.asia>
2020-10-13 02:50:49 +0900

    append project to organization

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   resources :organizations do
     resources :users
+    resources :projects do
+      resources :jobs
+    end
   end
-  resources :projects do
-    resources :jobs
-  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'auth/slack/callback', to: 'auth#callback'
 end
